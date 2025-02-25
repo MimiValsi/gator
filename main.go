02ac2ba4d5415eee0f3d62761558b6eae5bbaad1
Miguel DA SILVA <miguel@dasilvaf.net>
2025-02-25 16:20:44 +0100

    update main function with beggining of calls for login cmd

--- main.go
+++ main.go
@@ -3,22 +3,32 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/MimiValsi/gator/internal/config"
 )
 
 func main() {
 	s := new(state)
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %s\n", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
 
 	s.Config = &cfg
 
-	// cmds := commands{
-	// 	cli: make(map[string]func(*state, command) error),
-	// }
+	cmds := commands{
+		cli: make(map[string]func(*state, command) error),
+	}
 
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("usage: gator <cmd> <options>")
+		os.Exit(-1)
+	}
+
+	if args[1] == "login" {
+		cmds.cli[args[1]] = handlerLogin
+	}
 }
