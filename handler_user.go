098547cc608c5732477fd616bc9caf619a5f46d0
Miguel Da Silva <miguel@dasilvaf.net>
2025-02-27 21:20:59 +0100

    create handlerRegister func for registering new user to the db.

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/MimiValsi/gator/internal/database"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -16,5 +22,35 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	fmt.Println("User switched successfully!")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+
+	name := cmd.Args[0]
+	timeCreation := time.Now().UTC()
+	createUser := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: timeCreation,
+		UpdatedAt: timeCreation,
+		Name:      name,
+	}
+
+	ctx := context.Background()
+	_, err := s.db.CreateUser(ctx, createUser)
+	if err != nil {
+		return fmt.Errorf("user already exist, %w", err)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("couldn't register current user, %w", err)
+	}
+
+	fmt.Println("User created successfully!")
+
 	return nil
 }
