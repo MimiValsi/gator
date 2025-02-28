55dba3822eda828ecfdb08e6eb4dcd68040d9cca
Miguel DA SILVA <miguel@dasilvaf.net>
2025-02-28 18:57:23 +0100

    add truncateFeed() func before truncating users to avoid errors

--- handler_reset.go
+++ handler_reset.go
@@ -6,12 +6,18 @@ import (
 )
 
 func handlerReset(s *state, cmd command) error {
-	err := s.db.TruncateTable(context.Background())
+	ctx := context.Background()
+
+	err := s.db.TruncateFeeds(ctx)
 	if err != nil {
-		return fmt.Errorf("couldn't reset table: %w", err)
+		return fmt.Errorf("couln't reset feeds: %w", err)
+	}
+	err = s.db.TruncateUsers(ctx)
+	if err != nil {
+		return fmt.Errorf("couldn't reset users: %w", err)
 	}
 
-	fmt.Println("Table successfully reset!")
+	fmt.Println("Tables successfully reset!")
 
 	return nil
 }
