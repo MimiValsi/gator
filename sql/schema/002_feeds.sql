e592b7c5c32ae0a9548dc159a7863ded25e07810
Miguel DA SILVA <miguel@dasilvaf.net>
2025-02-28 14:48:52 +0100

    create schema command to create new feeds table in DB

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,13 @@
+-- +goose up
+CREATE TABLE feeds (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  name VARCHAR NOT NULL,
+  url VARCHAR NOT NULL UNIQUE,
+  user_id UUID NOT NULL REFERENCES users.id ON DELETE CASCADE
+);
+
+-- +goose down
+DROP TABLE feeds;
+
