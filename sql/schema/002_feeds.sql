78f6479758bb86ce8a59c93f045f840087eefd41
Miguel DA SILVA <miguel@dasilvaf.net>
2025-02-28 18:56:07 +0100

    typos and add foreign key constraint to user_id

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -1,13 +1,16 @@
--- +goose up
+-- +goose Up
 CREATE TABLE feeds (
   id UUID PRIMARY KEY,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
   name VARCHAR NOT NULL,
   url VARCHAR NOT NULL UNIQUE,
-  user_id UUID NOT NULL REFERENCES users.id ON DELETE CASCADE
+  user_id UUID NOT NULL,
+  CONSTRAINT fk_user 
+    FOREIGN KEY(user_id)
+      REFERENCES users(id) ON DELETE CASCADE
 );
 
--- +goose down
+-- +goose Down
 DROP TABLE feeds;
 
