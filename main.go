b9f5dc35620626f328d3e1e1f4db0b236e5cda25
Miguel DA SILVA <miguel@dasilvaf.net>
2025-03-12 13:20:31 +0100

    add unfollow command and handler

--- main.go
+++ main.go
@@ -61,6 +61,7 @@ func main() {
 	cmds.register("feeds", handlerListFeeds)
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("following", middlewareLoggedIn(handlerListFeedFollow))
+	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
 
 	err = cmds.run(progState, cmd)
 	if err != nil {
