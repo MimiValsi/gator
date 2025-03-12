c31cf474c9da6b0283bc57abde84e23394a9c380
Miguel DA SILVA <miguel@dasilvaf.net>
2025-03-12 10:07:13 +0100

    typo

--- handler_reset.go
+++ handler_reset.go
@@ -8,11 +8,7 @@ import (
 func handlerReset(s *state, cmd command) error {
 	ctx := context.Background()
 
-	err := s.db.DeleteFeedFollows(ctx)
-	if err != nil {
-		return fmt.Errorf("couldn't reset table: %w", err)
-	}
-	err = s.db.DeleteUsers(ctx)
+	err := s.db.DeleteUsers(ctx)
 	if err != nil {
 		return fmt.Errorf("couldn't reset table: %w", err)
 	}
