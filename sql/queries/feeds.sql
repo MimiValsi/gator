22f8ac7ffc3da5b5a353cbdc1eb234c533e4c2fa
Miguel DA SILVA <miguel@dasilvaf.net>
2025-03-12 10:07:54 +0100

    change query name and moved feed follow queries to another file

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -16,36 +16,8 @@ SELECT f.name, f.url, u.name
 LEFT JOIN users AS u 
   ON f.user_id = u.id;
 
--- name: GetFeed :one
+-- name: GetFeedByURL :one
 SELECT id, created_at, updated_at, name, url, user_id
   FROM feeds
 WHERE url = $1;
 
--- name: CreateFeedFollow :many
-WITH inserted_feed_follow AS (
-  INSERT INTO feed_follows (
-    id,
-    created_at,
-    updated_at,
-    user_id,
-    feed_id
-  )
-  VALUES ($1, $2, $3, $4, $5)
-  RETURNING *
-)
-SELECT
-  inserted_feed_follow.*,
-  feeds.name AS feed_name,
-  users.name AS user_name
-FROM inserted_feed_follow
-INNER JOIN feeds ON feeds.id = inserted_feed_follow.feed_id
-INNER JOIN users ON users.id = inserted_feed_follow.user_id;
-
--- name: GetFollowingUsers :many
-SELECT feeds.name AS feed_name
-  FROM feed_follows
-INNER JOIN feeds ON feeds.id = feed_follows.feed_id
-  WHERE feed_follows.user_id = $1;
-
--- name: DeleteFeedFollows :exec
-DELETE FROM feed_follows CASCADE;
