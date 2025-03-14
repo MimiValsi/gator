bddf76bee50763f7fe00815eb10fe06e294f621f
Miguel DA SILVA <miguel@dasilvaf.net>
2025-03-14 13:14:00 +0100

    add function to fetch known feeds (in db) and return the oldest one. Call fetchRSS with last feed url and print it.

--- handler_agg.go
+++ handler_agg.go
@@ -2,17 +2,63 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
+
+	"github.com/MimiValsi/gator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	ctx := context.Background()
-	url := "https://www.wagslane.dev/index.xml"
-	rssFeed, err := fetchRSS(ctx, url)
+	if len(cmd.Args) != 1 {
+		fmt.Printf("usage: %s <arg>", cmd.Name)
+		fmt.Println("Exemple: arg 30s")
+		return nil
+	}
+	
+	timeBetweenReqs, err := time.ParseDuration(cmd.Args[0])
 	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %w", err)
+		return fmt.Errorf("Invalid time format: %w", err)
+	}
+	
+	fmt.Printf("Collection feeds every %v\n", timeBetweenReqs)
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ;; <-ticker.C {
+		scrapeFeeds(s)
+	}
+	
+	//return nil
+}
+
+func scrapeFeeds(s *state) error {
+	ctx := context.Background()
+	feed, err := s.db.GetNextFeedtoFetch(ctx)
+	if err != nil {
+		return fmt.Errorf("couldn't get next feed fetch: %w", err)
 	}
 
-	fmt.Printf("%+v\n", rssFeed)
+	tn := time.Now().UTC()
+	markFeed := database.MarkFeedFetchedParams{
+		UpdatedAt: tn,
+		LastFetchedAt: sql.NullTime{
+			Time: tn,
+			Valid: true,
+		},
+		ID: feed.ID,
+	}
+
+	err = s.db.MarkFeedFetched(ctx, markFeed)
+	if err != nil {
+		return fmt.Errorf("couldn't mark fetched feed: %w", err)
+	}
+
+	rss, err := fetchRSS(ctx, feed.Url)
+	if err != nil {
+		return fmt.Errorf("couldn't fetch rss: %w", err)
+	}
+
+	for _, item := range rss.Channel.Items {
+		fmt.Println(item.Title)
+	}
 	return nil
 }
