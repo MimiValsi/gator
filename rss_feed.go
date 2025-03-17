3ae47e3a7cea6b27a1339d2382ed68cc78a9b242
Miguel DA SILVA <miguel@dasilvaf.net>
2025-03-17 19:28:34 +0100

    bootdev corrections

--- rss_feed.go
+++ rss_feed.go
@@ -14,7 +14,7 @@ type RSSFeed struct {
 		Title       string    `xml:"title"`
 		Link        string    `xml:"link"`
 		Description string    `xml:"description"`
-		Items        []RSSItem `xml:"items"`
+		Item        []RSSItem `xml:"item"`
 	} `xml:"channel"`
 }
 
@@ -26,7 +26,7 @@ type RSSItem struct {
 }
 
 func fetchRSS(ctx context.Context, feedURL string) (*RSSFeed, error) {
-	httpClient := &http.Client{
+	httpClient := http.Client{
 		Timeout: 10 * time.Second,
 	}
 
@@ -34,8 +34,8 @@ func fetchRSS(ctx context.Context, feedURL string) (*RSSFeed, error) {
 	if err != nil {
 		return nil, err
 	}
+	
 	req.Header.Set("User-Agent", "gator")
-
 	resp, err := httpClient.Do(req)
 	if err != nil {
 		return nil, err
@@ -47,8 +47,8 @@ func fetchRSS(ctx context.Context, feedURL string) (*RSSFeed, error) {
 		return nil, err
 	}
 
-	rssFeed := &RSSFeed{}
-	err = xml.Unmarshal(data, rssFeed)
+	var rssFeed RSSFeed
+	err = xml.Unmarshal(data, &rssFeed)
 	if err != nil {
 		return nil, err
 	}
@@ -56,10 +56,11 @@ func fetchRSS(ctx context.Context, feedURL string) (*RSSFeed, error) {
 	rssFeed.Channel.Title = html.UnescapeString(rssFeed.Channel.Title)
 	rssFeed.Channel.Description = html.UnescapeString(rssFeed.Channel.Description)
 
-	for i := range rssFeed.Channel.Items {
-		rssFeed.Channel.Items[i].Title = html.UnescapeString(rssFeed.Channel.Items[i].Title)
-		rssFeed.Channel.Items[i].Description = html.UnescapeString(rssFeed.Channel.Items[i].Description)
+	for i, item := range rssFeed.Channel.Item {
+		item.Title = html.UnescapeString(item.Title)
+		item.Description = html.UnescapeString(item.Description)
+		rssFeed.Channel.Item[i] = item
 	}
 
-	return rssFeed, nil
+	return &rssFeed, nil
 }
