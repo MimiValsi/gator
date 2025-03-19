f762c9d865220ad61ff71867b2fba6c91a43f8e3
Miguel DA SILVA <miguel@dasilvaf.net>
2025-03-19 20:42:24 +0100

    remove printing statements. add function to create a new post in DB for everypost found by URL

--- handler_agg.go
+++ handler_agg.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/MimiValsi/gator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -43,7 +46,25 @@ func scrapeFeeds(s *state) error {
 	}
 
 	for _, item := range rss.Channel.Item {
-		fmt.Println(item.Title)
+		tn := time.Now().UTC()
+		pubdate, err := time.Parse(time.RFC1123, item.PubDate)
+		if err != nil {
+			return fmt.Errorf("couldn't parse date format: %w", err)
+		}
+		createPostParam := database.CreatePostParams{
+			ID: uuid.New(),
+			CreatedAt: tn,
+			UpdatedAt: tn,
+			Title: item.Title,
+			Url: feed.Url,
+			Description: item.Description,
+			PublishedAt: pubdate,
+			FeedID: feed.ID,
+		}
+		_, err = s.db.CreatePost(ctx, createPostParam)
+		if err != nil {
+			return fmt.Errorf("couldn't create post on db: %w", err)
+		}
 	}
 	fmt.Printf("Feed %s collected, %v posts found\n", feed.Name, len(rss.Channel.Item))
 	return nil
