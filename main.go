18f3038edb8bbec55e68aab9e0eccf9d3e868a88
Miguel DA SILVA <miguel@dasilvaf.net>
2025-03-19 20:43:45 +0100

    add new command. browse cmd

--- main.go
+++ main.go
@@ -62,6 +62,7 @@ func main() {
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("following", middlewareLoggedIn(handlerListFeedFollow))
 	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
+	cmds.register("browse", handlerBrowse)
 
 	err = cmds.run(progState, cmd)
 	if err != nil {
