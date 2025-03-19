67d0ee98b4993c9c038607b1565a95a4c0ebaa49
Miguel DA SILVA <miguel@dasilvaf.net>
2025-03-19 20:50:02 +0100

    bootdev correction

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -5,6 +5,10 @@ RETURNING *;
 --
 
 -- name: GetPostsPerUser :many
-SELECT * FROM posts
-LIMIT $1;
+SELECT posts.*, feeds.name AS feed_name FROM posts
+JOIN feed_follows ON feed_follows.feed_id = posts.feed_id
+JOIN feeds ON posts.feed_id = feeds.id
+WHERE feed_follow.user_id = $1
+ORDER BY posts.published_at DESC
+LIMIT $2;
 --
